c51419660ba00a642a14f5b3b30fd5b0c74ba4d0
Iñaki Tuya Rodríguez <uo289909@uniovi.es>
2025-05-29 12:20:18 +0300

    Creating interfaces for book APIs responses (both API for now until deciding which one works better)

---
+++ src/infrastructure/interfaces/open-library.responses.ts
@@ -0,0 +1,64 @@
+
+export interface OpenLibraryResponseByIsbn {
+    'ISBN:9780974872407': BookData;
+}
+
+export interface BookData {
+    url:             string;
+    key:             string;
+    title:           string;
+    authors:         Author[];
+    number_of_pages: number;
+    pagination:      string;
+    by_statement:    string;
+    identifiers:     Identifiers;
+    publishers:      Publish[];
+    publish_places:  Publish[];
+    publish_date:    string;
+    subjects:        Author[];
+    subject_places:  Author[];
+    subject_people:  Author[];
+    subject_times:   Author[];
+    notes:           string;
+    links:           Link[];
+    ebooks:          Ebook[];
+    cover:           Cover;
+}
+
+export interface Author {
+    url:  string;
+    name: string;
+}
+
+export interface Cover {
+    small:  string;
+    medium: string;
+    large:  string;
+}
+
+export interface Ebook {
+    preview_url:  string;
+    availability: string;
+    formats:      Formats;
+}
+
+export interface Formats {
+}
+
+export interface Identifiers {
+    goodreads:    string[];
+    librarything: string[];
+    isbn_10:      string[];
+    isbn_13:      string[];
+    oclc:         string[];
+    openlibrary:  string[];
+}
+
+export interface Link {
+    title: string;
+    url:   string;
+}
+
+export interface Publish {
+    name: string;
+}

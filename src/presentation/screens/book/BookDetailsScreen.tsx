64146f467b9f8d15c7911c2da15419175a17b1c4
Iñaki Tuya Rodríguez <uo289909@uniovi.es>
2025-07-23 16:21:47 +0300

    Fixed book details screen

--- src/presentation/screens/book/BookDetailsScreen.tsx
+++ src/presentation/screens/book/BookDetailsScreen.tsx
@@ -9,11 +9,11 @@ export const BookDetailsScreen = () => {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', padding: 20 }}>
-      <Image source={{ uri: book.cover }} style={{ width: 200, height: 300, borderRadius: 12, marginBottom: 20 }} />
+      <Image source={{ uri: book.cover_url || 'https://placehold.co/200x300.png?text=No+Cover' }} style={{ width: 200, height: 300, borderRadius: 12, marginBottom: 20 }} />
       <Text style={{ fontSize: 28, fontWeight: 'bold', marginBottom: 10 }}>{book.title}</Text>
-      <Text style={{ fontSize: 20, marginBottom: 10 }}>{book.authors.join(', ')}</Text>
-      <Text style={{ fontSize: 16, marginBottom: 10 }}>Páginas: {book.pages}</Text>
-      <Text style={{ fontSize: 16, marginBottom: 10 }}>Publicado: {book.publishDate}</Text>
+      <Text style={{ fontSize: 20, marginBottom: 10 }}>{book.author}</Text>
+      <Text style={{ fontSize: 16, marginBottom: 10 }}>Nº de páginas: {book.pages}</Text>
+      <Text style={{ fontSize: 16, marginBottom: 10 }}>Publicado en {book.release_year}</Text>
     </View>
   );
 };
